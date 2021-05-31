216e9a3f44424e394bebe840ae14ca771b7f76e4
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-05-31 17:31:02 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import './styles.css';
 import menu from './menu.json';
 import menuCardsTpl from './templates/menu.hbs';
+
+// const menuRef = document.querySelector('.js-menu');
+
+// const markup = menuCardsTpl(menu);
+
+// menuRef.insertAdjacentHTML('beforeend', markup);
 
 console.log(menuCardsTpl(menu));
