8371ed398c5bad28434dd4a423969177ecd939a8
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-05-31 19:46:14 +0300

    переключатель темы

--- src/index.js
+++ src/index.js
@@ -3,9 +3,32 @@ import menu from './menu.json';
 import menuCardsTpl from './templates/menu.hbs';
 
 const menuRef = document.querySelector('.js-menu');
+const menuMarkup = menuCardsTpl(menu);
+menuRef.insertAdjacentHTML('beforeend', menuMarkup);
 
-const markup = menuCardsTpl(menu);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-menuRef.insertAdjacentHTML('beforeend', markup);
+const bodyRef = document.querySelector('body');
+const switchEl = document.querySelector('#theme-switch-toggle');
+switchEl.addEventListener('change', onChangedSwitch);
 
-// console.log(menuCardsTpl(menu));
+if (localStorage.getItem('theme') === Theme.DARK) {
+  bodyRef.classList.add(Theme.DARK);
+  switchEl.checked = 'true';
+} else {
+  localStorage.setItem('theme', Theme.LIGHT);
+  bodyRef.classList.add(Theme.LIGHT);
+}
+
+function onChangedSwitch() {
+  if (localStorage.theme === Theme.LIGHT) {
+    bodyRef.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
