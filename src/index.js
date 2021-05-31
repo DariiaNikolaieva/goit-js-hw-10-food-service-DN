5e26b0e54e131178f8cb321791ac85f876ed2189
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-05-31 18:08:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './styles.css';
 import menu from './menu.json';
 import menuCardsTpl from './templates/menu.hbs';
 
-// const menuRef = document.querySelector('.js-menu');
+const menuRef = document.querySelector('.js-menu');
 
-// const markup = menuCardsTpl(menu);
+const markup = menuCardsTpl(menu);
 
-// menuRef.insertAdjacentHTML('beforeend', markup);
+menuRef.insertAdjacentHTML('beforeend', markup);
 
-console.log(menuCardsTpl(menu));
+// console.log(menuCardsTpl(menu));
